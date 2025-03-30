f6b06eeecde7a6e295193dba061eeac01f6cd7c6
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-30 13:07:44 -0300

    first commit

---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
