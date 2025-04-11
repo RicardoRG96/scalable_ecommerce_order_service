83de26bee015ea4fda715e0a53c7e19e12e322e9
RicardoRG96 <ricardoretamal10@gmail.com>
2025-04-11 05:39:48 -0400

    add testGetOrderItemByOrderId to OrderItemController

--- src/test/java/com/ricardo/scalable/ecommerce/platform/order_service/integrationTests/OrderItemControllerTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/order_service/integrationTests/OrderItemControllerTest.java
@@ -74,6 +74,50 @@ public class OrderItemControllerTest {
 	}
 
     @Test
+	@Order(3)
+	void testGetOrderItemsByOrderId() {
+		client.get()
+			.uri("/order-items/order/2")
+			.exchange()
+			.expectStatus().isOk()
+			.expectHeader().contentType(MediaType.APPLICATION_JSON)
+			.expectBody()
+			.consumeWith(res -> {
+				try {
+					JsonNode json = objectMapper.readTree(res.getResponseBody());
+					assertAll(
+						() -> assertNotNull(json),
+						() -> assertTrue(json.isArray()),
+						() -> assertEquals(2, json.size()),
+						() -> assertEquals(3L, json.get(0).path("id").asLong()),
+						() -> assertEquals(2L, json.get(0).path("order").path("id").asLong()),
+						() -> assertEquals(3L, json.get(0).path("productSku").path("id").asLong()),
+						() -> assertEquals(3, json.get(0).path("quantity").asInt()),
+						() -> assertEquals(29.99, json.get(0).path("unitPrice").asDouble()),
+						() -> assertEquals(1L, json.get(0).path("discount").path("id").asLong()),
+						() -> assertEquals(4L, json.get(1).path("id").asLong()),
+						() -> assertEquals(2L, json.get(1).path("order").path("id").asLong()),
+						() -> assertEquals(7L, json.get(1).path("productSku").path("id").asLong()),
+						() -> assertEquals(2, json.get(1).path("quantity").asInt()),
+						() -> assertEquals(19.99, json.get(1).path("unitPrice").asDouble()),
+						() -> assertEquals(3L, json.get(1).path("discount").path("id").asLong())		
+					);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+	}
+
+	@Test
+	@Order(4)
+	void testGetOrderItemsByOrderIdNotFound() {
+		client.get()
+			.uri("/order-items/order/999")
+			.exchange()
+			.expectStatus().isNotFound();
+	}
+
+    @Test
     void testProfile() {
         String[] activeProfiles = env.getActiveProfiles();
         assertArrayEquals(new String[] { "test" }, activeProfiles);
